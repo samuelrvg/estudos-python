c9cbb9ced3a734f16c02d5e5fb384342cde7c0ad
Samuel Vaz <samuel.rvg@gmail.com>
2023-12-11 21:53:09 -0300

    variaveis

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+## Estudos Python
+
+https://www.youtube.com/watch?v=-VeVq64Fgw0
---
+++ variaveis.py
@@ -0,0 +1,26 @@
+nome = 'Samuel'
+print(nome)
+
+media = 0
+n1 = n2 = n3 = n4 = 0.0
+nome, idade = 'Samuel', 26
+estado = True
+print(media)
+
+# Função type()
+print(type(media))
+print(type(n2))
+print(type(nome))
+print(type(estado))
+print(type(1 + 2j)) # número complexo
+
+# Função isistance()
+a = 10
+b = 'Sol'
+print(isinstance(a, (int, float)))
+print(isinstance(b, str))
+
+a = 40
+c = 3
+r = a * c
+print(r)
